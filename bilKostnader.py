85121fd12f79e703a369e8e56c0ab7f035f0b10a
Atilla <fjeldstad@gmail.com>
2025-08-29 09:39:27 +0200

    Update bilKostnader.py
    Lagt inn strømprisen også. Relevant for elbil kostnader.

--- bilKostnader.py
+++ bilKostnader.py
@@ -1,9 +1,11 @@
-kmPerAr = 10000
-traffikavgift = 8.38 * 365  # 8.38 per dag i 1 år
-eForsikring = 5000
-bForsikring = 7500
-eDrivstoff = 0.2 * kmPerAr  # kWh/km
-bDrivstoff = 1 * kmPerAr    # 1kr per km
+kmPerAr = 10000                     # km/år
+traffikavgift = 8.38 * 365          # 8.38 per dag i 1 år
+eForsikring = 5000                  # kr/år
+bForsikring = 7500                  # kr/år
+stromPris = 2.00                    # kr/kWh
+stromPrisPerKm = stromPris * 0.2    # kWh/km
+eDrivstoff = stromPrisPerKm * kmPerAr      
+bDrivstoff = 1 * kmPerAr            # 1kr per km
 eBom = 0.1 * kmPerAr        
 bBom = 0.3 * kmPerAr
 
@@ -11,5 +13,5 @@ eKostnad = traffikavgift + eForsikring + eDrivstoff + eBom
 bKostnad = traffikavgift + bForsikring + bDrivstoff + bBom
 
 print("I løpet av ett år vil en elbil koste " + str(eKostnad) + ",-")
-print("sammenlignet vil en bensinbil koste  " + str(bKostnad) + ",-")
+print("sammenlignet vil en bensinbil koste " + str(bKostnad) + ",-")
 print("Differansen er " + str(round(max(bKostnad,eKostnad)-min(bKostnad,eKostnad))) + ".")
