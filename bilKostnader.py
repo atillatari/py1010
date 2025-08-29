39b717f80cbd8b9cd5c6c17b1ed469d6dd046575
Atilla <fjeldstad@gmail.com>
2025-08-29 09:07:10 +0200

    Create bilKostnader.py
    Oppgave 1 
    for Py1010 høst 2025

---
+++ bilKostnader.py
@@ -0,0 +1,14 @@
+kmPerAr = 10000
+traffikavgift = 8.38 * 365  # 8.38 per dag i 1 år
+eForsikring = 5000
+bForsikring = 7500
+eDrivstoff = 0.2 * kmPerAr  # kWh/km
+bDrivstoff = 1 * kmPerAr    # 1kr per km
+eBom = 0.1 * kmPerAr        
+bBom = 0.3 * kmPerAr
+
+eKostnad = str(traffikavgift + eForsikring + eDrivstoff + eBom)
+bKostnad = str(traffikavgift + bForsikring + bDrivstoff + bBom)
+
+print("I løpet av ett år vil en elbil koste " + eKostnad + ",-")
+print("sammenlignet vil en bensinbil koste  " + bKostnad + ",-.")
