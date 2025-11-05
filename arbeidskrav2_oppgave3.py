4660abedff3436addb77a362e36c4e76246f220f
Atilla <fjeldstad@gmail.com>
2025-11-05 19:05:29 +0100

    Refactor header comments and maintain functionality

--- arbeidskrav2_oppgave3.py
+++ arbeidskrav2_oppgave3.py
@@ -1,7 +1,5 @@
 """
 Arbeidskrav 2 - Oppgave 3
-
-Cathrine L. Fjeldstad (hei@calif.no)
 Oppdatert: Tue Oct  7 17:46:15 2025
 """
 
@@ -10,5 +8,6 @@ import numpy as np  # Importerer numpy for å bruke pi
 v_grad = float(input("Skriv inn gradtallet: "))  # Brukeren skriver inn grader
 
 v_rad = v_grad*np.pi/180  # Konverterer grader til radianer
+
 
 print(f"{v_grad} grader tilsvarer {v_rad:.4f} radianer.")  # Skriver ut resultatet til konsoll
