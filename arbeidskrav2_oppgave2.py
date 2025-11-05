6b9897e3e0e6ed83deef3fa8aea2546634f8d9f0
Atilla <fjeldstad@gmail.com>
2025-11-05 19:05:04 +0100

    Remove unnecessary comments and clean up code

--- arbeidskrav2_oppgave2.py
+++ arbeidskrav2_oppgave2.py
@@ -1,11 +1,10 @@
 """
 Arbeidskrav 2 - Oppgave 2
-
-Cathrine L. Fjeldstad (hei@calif.no)
 Oppdatert: Tue Oct  7 17:41:06 2025
 """
 import math
 
 antall_elever = int(input("Hvor mange elever skal ha pizza? "))
 antall_pizza = math.ceil(antall_elever/4)  #ceil-funksjonen runder opp til nærmeste heltall
+
 print("Det må kjøpes inn", antall_pizza, "pizzaer.")
