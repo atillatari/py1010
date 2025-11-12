e484b0fe5a0d7af29f0726c10925a841091f6da9
Atilla <fjeldstad@gmail.com>
2025-11-12 12:37:05 +0100

    La til areal

--- arbeidskrav2_oppgave5.py
+++ arbeidskrav2_oppgave5.py
@@ -4,11 +4,16 @@ Oppdatert: Wed Nov  5 18:25:12 2025
 """
 import numpy as np
 
-a = int(input("Verdien av a er? : "))
-b = int(input("Verdien av b er? : "))
-omkrets_sirkel = 2*np.pi*a
+a = float(input("Verdien av a er? : "))
+b = float(input("Verdien av b er? : "))
+
+areal_trekant = (a*b)/2
+areal_sirkel = np.pi*((a**2)/2)
+areal_figur = areal_trekant + (areal_sirkel/2)
+print("Figurens areal er: ", areal_figur)
+
+omkrets_sirkel = np.pi*a
 hypotenus = np.sqrt(a**2 + b**2)
 omkrets_trekant = a+b+hypotenus
 omkrets_figur = omkrets_sirkel/2 + omkrets_trekant-a
-print(omkrets_figur)
-
+print("Figurens omkrets er:", omkrets_figur)
